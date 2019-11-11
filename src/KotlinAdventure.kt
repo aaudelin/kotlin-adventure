7b20ee2658a892ba102b55b0b33e61ff2718fb9e
Aurélien Audelin <aaudelin@tradelab.fr>
2019-11-11 17:46:52 +0100

    Exercice 1 - init

---
+++ src/KotlinAdventure.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+// Deux variables pour indiquer les dimensions du plateau de jeu (rectangulaire)
+const val NB_LIGNES = 10
+const val NB_COLONNES = 10
+
+// Une variable pour stocker la date de début du jeu
+val dateDebut = Date()
+
+// Variable pour compter le nombre de tours à la fin du jeu
+var nbTours = 0
