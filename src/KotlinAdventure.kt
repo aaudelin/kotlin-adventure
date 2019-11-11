2d257807fc2ae06742fa7856a06233c801ceffe6
Aurélien Audelin <aaudelin@tradelab.fr>
2019-11-11 18:25:17 +0100

    Exercice 2 - init

--- src/KotlinAdventure.kt
+++ src/KotlinAdventure.kt
@@ -14,28 +14,21 @@ var nbTours = 0
 
 // Créer un tableau de monstres avec une taille de 5
 // Remplir ce tableau avec 10 monstres nommés "MONSTREX" avec X = 1..5
-val monstres = arrayOf("MONSTRE1", "MONSTRE2", "MONSTRE3", "MONSTRE4", "MONSTRE5")
-
 
 // Créer un tableau de chasseurs avec une taille de 5
 // Remplir ce tableau avec 5 chasseurs nommés "CHASSEURX" avec X = 1..5
-val chasseurs = arrayOf("CHASSEUR1", "CHASSEUR2", "CHASSEUR3", "CHASSEUR4", "CHASSEUR5")
 
 // Créer un tableau d'armes avec une taille de 8
 // Remplir ce tableau avec
 //     - "LEGENDAIREX" pour 2 armes avec X = 1..2
 //     - "ARMEX" pour 6 armes avec X = 1..6
-val armes = arrayOf("LENGENDAIRE1", "LENGENDAIRE2",
-    "ARME1", "ARME2", "ARME3", "ARME4", "ARME5", "ARME6")
 
 // Créer une matrice jeu avec pour indices i et j la position de l'objet sur le plateur
 // et pour valeur le nom de l'objet.
 // Positionner dans l'ordre d'abord les montres, les chasseurs puis les armes légendaires
 // et enfin les armes classiques.
-val jeu = arrayOf(
-        arrayOf(*monstres, *chasseurs),
-        arrayOf(*armes)
-    )
+val jeu =
+
 // Le résultat attendu est
 // MONSTRE1 MONSTRE2 .... MONSTRE5 CHASSEUR1 CHASSEUR2 ... CHASSEUR5
 // LENGENDAIRE1 LEGENDAIRE2 ARME1 .. ARME6
